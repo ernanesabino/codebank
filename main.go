adee38af8c4dabec147cb1b36faa58af010c9f72
Ernane Sabino <esabino@stefanini.com>
2022-08-16 16:21:55 -0300

    CB :: Adicionando gRPC e Kafka

--- main.go
+++ main.go
@@ -4,46 +4,54 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/ernanesabino/codebank/domain"
+	"github.com/ernanesabino/codebank/infrastructure/grpc/server"
+	"github.com/ernanesabino/codebank/infrastructure/kafka"
 	"github.com/ernanesabino/codebank/infrastructure/repository"
 	"github.com/ernanesabino/codebank/usecase"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
 
 func main() {
 	db := setupDb()
 	defer db.Close()
 
-	cc := domain.NewCreditCard()
-	cc.Number = "1234"
-	cc.Name = "Ernane"
-	cc.ExpirationYear = 2028
-	cc.ExpirationMounth = 7
-	cc.CVV = 123
-	cc.Limit = 1000
-	cc.Balance = 0
-
-	repo := repository.NewTransactionRepositoryDb(db)
-	err := repo.CreateCreditCard(*cc)
-	if err != nil {
-		fmt.Println(err)
-	}
+	producer := setupKafkaProducer()
+	processTransactionUseCase := setupTransactionUseCase(db, producer)
+	fmt.Println("Rodando gRPC Server")
+	serveGRPC(processTransactionUseCase)
+	
 }
 
-func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+func setupTransactionUseCase(db *sql.DB, producer kafka.KafkaProducer) usecase.UseCaseTransaction {
 	transactionRepository := repository.NewTransactionRepositoryDb(db)
 	useCase := usecase.NewUseCaseTransaction(transactionRepository)
+	useCase.KafkaProducer = producer
 	return useCase
+}
+
+func setupKafkaProducer() kafka.KafkaProducer {
+	producer := kafka.NewKafkaProducer()
+	producer.SetupProducer(os.Getenv("KafkaBootstrapServers"))
+	return producer
 }
 
 func setupDb() *sql.DB {
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		"db",
-		"5432",
-		"postgres",
-		"root",
-		"codebank",
+		os.Getenv("host"),
+		os.Getenv("port"),
+		os.Getenv("user"),
+		os.Getenv("password"),
+		os.Getenv("dbname"),
 	)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
@@ -51,3 +59,24 @@ func setupDb() *sql.DB {
 	}
 	return db
 }
+
+func serveGRPC(processTransactionUseCase usecase.UseCaseTransaction) {
+	grpcServer := server.NewGRPCServer()
+	grpcServer.ProcessTransactionUseCase = processTransactionUseCase
+	grpcServer.Serve()
+}
+
+// cc := domain.NewCreditCard()
+// cc.Number = "1234"
+// cc.Name = "Ernane"
+// cc.ExpirationYear = 2028
+// cc.ExpirationMounth = 7
+// cc.CVV = 123
+// cc.Limit = 1000
+// cc.Balance = 0
+
+// repo := repository.NewTransactionRepositoryDb(db)
+// err := repo.CreateCreditCard(*cc)
+// if err != nil {
+// 	fmt.Println(err)
+// }
